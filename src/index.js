d1388ff72d4b9794482ebe8df9bc7c9af2647428
Bryan <aliceabryan1996@gmail.com>
2024-03-14 16:13:03 -0400

    completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form")
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    createTask(event.target.new_task_description.value)
+    form.reset()
+  })
 });
+
+function createTask(task){
+  let pTag = document.createElement('p')
+  let button = document.createElement('button')
+  button.addEventListener('click', deleteTask)
+  button.textContent = "X"
+  pTag.textContent = `* ${task}  `
+  pTag.appendChild(button)
+  document.querySelector("#tasks").appendChild(pTag)
+  
+}
+
+function deleteTask(event){
+  event.target.parentNode.remove()
+}
